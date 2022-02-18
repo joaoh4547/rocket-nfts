4d2046d40f276353b257c0f913155bc4fb59de69
João Henrique <joaohenriquetdj@gmail.com>
2022-02-18 20:35:17 -0300

    update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 import { FC } from "react";
+import { useMediaQuery } from "react-responsive";
 import { Banner } from "./Banner";
 import { Gallery } from "./Gallery";
 import styles from "./Header.module.scss";
 import { Navbar } from "./NavBar";
 
 export const Header: FC = () => {
+  const isLargeScreen = useMediaQuery({ query: "(min-width: 1200px)" });
+
   return (
     <div className={styles.container}>
       <Navbar />
       <Banner />
-      <Gallery />
+      {isLargeScreen && <Gallery />}
     </div>
   );
 };
